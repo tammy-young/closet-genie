95cbe045c2c450c3c0c4cbb8e7c4d09197747642
Tammy Young <tammy.q.young@gmail.com>
2025-05-06 14:09:59 -0600

    feat: add get closet api

---
+++ backend/api/getCloset.js
@@ -0,0 +1,27 @@
+import 'dotenv/config';
+
+const closetPageURL = (id, index) => { return `http://www.stardoll.com/en/com/user/getSuiteFiltered.php?id=${id}&storage&storagePage=${index}` }
+
+async function getCloset(userId) {
+  let closetItems = [];
+  let storageIndex = 1;
+
+  while (true) {
+    let response = await fetch(closetPageURL(userId, storageIndex));
+    let pageData = await response.json();
+
+    // no more items
+    if (pageData.items.item.length === 0) {
+      break;
+    }
+
+    // add items to closetItems
+    closetItems = closetItems.concat(pageData.items.item);
+
+    storageIndex++;
+  }
+
+  return closetItems;
+}
+
+export default getCloset;
---
+++ backend/index.js
@@ -0,0 +1,41 @@
+import express from 'express';
+import 'dotenv/config';
+import getCloset from './api/getCloset.js';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+app.use(cors({ origin: process.env.ALLOWED_ORIGIN }));
+
+app.use(function (req, res, next) {
+  const origin = req.headers.origin;
+  if (origin !== process.env.ALLOWED_ORIGIN) {
+    res.status(403).send('403 Forbidden');
+    return;
+  }
+  next();
+});
+
+app.get("/getCloset", (req, res) => {
+  let userId = req.query.userId;
+  getCloset(userId)
+    .then(data => {
+      res.setHeader("Content-Type", "application/json")
+      res.send(JSON.stringify(data, null, 2));
+    })
+    .catch((e) => {
+      console.log("Error in getCloset: " + e);
+    });
+});
+
+app.get("/", async (req, res) => {
+  res.send(JSON.stringify({ "message": "hello closet genie api" }, null, 2));
+});
+
+app.listen(PORT, (error) => {
+  if (!error)
+    console.log("Server is Successfully Running, and App is listening on port " + PORT)
+  else
+    console.log("Error occurred, server can't start", error);
+}
+);
